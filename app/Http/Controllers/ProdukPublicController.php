0f5b7aba5c44a674e8001286819c1934ea7b1e75
Haekalgtg <haekalgiffari1140@gmail.com>
2026-07-28 18:24:33 +0700

    feat: Add Produk Catalogue with UMKM integration

--- app/Http/Controllers/ProdukPublicController.php
+++ app/Http/Controllers/ProdukPublicController.php
@@ -3,14 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produk;
+use App\Models\Umkm;
+use App\Models\Kategori;
+use Illuminate\Http\Request;
 
 class ProdukPublicController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $produks = Produk::with(['umkm', 'kategori'])->latest()->paginate(12);
+        $query = Produk::with(['umkm', 'kategori']);
 
-        return view('produk.index', compact('produks'));
+        if ($request->filled('cari')) {
+            $query->where('nama_produk', 'like', '%' . $request->cari . '%');
+        }
+
+        if ($request->filled('umkm')) {
+            $query->whereHas('umkm', function ($q) use ($request) {
+                $q->where('slug', $request->umkm);
+            });
+        }
+
+        if ($request->filled('kategori')) {
+            $query->where('kategori_id', $request->kategori);
+        }
+
+        $produks = $query->latest()->paginate(12)->withQueryString();
+
+        $umkmList = Umkm::orderBy('nama_umkm')->get(['id', 'slug', 'nama_umkm']);
+        $kategoriList = Kategori::orderBy('nama')->get(['id', 'nama']);
+
+        return view('produk.index', compact('produks', 'umkmList', 'kategoriList'));
     }
 
     public function show(string $slug)
@@ -19,6 +41,12 @@ class ProdukPublicController extends Controller
             ->where('slug', $slug)
             ->firstOrFail();
 
-        return view('produk.show', compact('produk'));
+        $produkLain = Produk::where('umkm_id', $produk->umkm_id)
+            ->where('id', '!=', $produk->id)
+            ->latest()
+            ->take(5)
+            ->get();
+
+        return view('produk.show', compact('produk', 'produkLain'));
     }
 }
